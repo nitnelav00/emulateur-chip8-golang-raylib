6a6c924e59ff74d5011f34e643654533e68a43b8
nitnelav00 <65456609+nitnelav00@users.noreply.github.com>
2023-10-29 11:12:04 +0100

    Update main.go

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import rl "github.com/gen2brain/raylib-go/raylib"
 
 func main() {
 	rend := New_Render()
-	cpu := New_CPU(10, rend, "aaaa.bin")
+	cpu := New_CPU(10, rend, "program.ch8")
 
 	rl.InitWindow(SCREEN_SIZE_X*PIXEL_SCALE, SCREEN_SIZE_Y*PIXEL_SCALE, "emutest")
 	defer rl.CloseWindow()
